6aafefc90bc1b95e21943f2eaad02d764f38df1d
SJL <3329318812@qq.com>
2020-08-07 08:02:28 +0800

    day06作业

---
+++ day06/ssm-05/src/main/java/com/xiaoshu/controller/PersonController.java
@@ -0,0 +1,157 @@
+package com.xiaoshu.controller;
+
+import java.net.URLEncoder;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.xiaoshu.config.util.ConfigUtil;
+import com.xiaoshu.entity.Company;
+import com.xiaoshu.entity.Operation;
+import com.xiaoshu.entity.Person;
+import com.xiaoshu.entity.PersonVo;
+import com.xiaoshu.entity.Role;
+import com.xiaoshu.entity.User;
+import com.xiaoshu.service.CompanyService;
+import com.xiaoshu.service.OperationService;
+import com.xiaoshu.service.PersonService;
+import com.xiaoshu.service.RoleService;
+import com.xiaoshu.service.UserService;
+import com.xiaoshu.util.StringUtil;
+import com.xiaoshu.util.TimeUtil;
+import com.xiaoshu.util.WriterUtil;
+
+import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.PageInfo;
+
+@Controller
+@RequestMapping("person")
+public class PersonController extends LogController{
+	static Logger logger = Logger.getLogger(PersonController.class);
+
+	
+	
+	@Autowired
+	private RoleService roleService ;
+	@Autowired
+	private PersonService personService;
+	
+	@Autowired
+	private CompanyService  companyService ;
+	@Autowired
+	private OperationService operationService;
+	
+	
+	@RequestMapping("personIndex")
+	public String index(HttpServletRequest request,Integer menuid) throws Exception{
+		List<Role> roleList = roleService.findRole(new Role());
+		List<Operation> operationList = operationService.findOperationIdsByMenuid(menuid);
+		List<Company> clist=companyService.findCompany();
+		request.setAttribute("clist", clist);
+		request.setAttribute("operationList", operationList);
+		request.setAttribute("roleList", roleList);
+		return "person";
+	}
+	
+	
+	@RequestMapping(value="userList",method=RequestMethod.POST)
+	public void userList(PersonVo personVo,HttpServletRequest request,HttpServletResponse response,String offset,String limit) throws Exception{
+		try {
+			
+			Integer pageSize = StringUtil.isEmpty(limit)?ConfigUtil.getPageSize():Integer.parseInt(limit);
+			Integer pageNum =  (Integer.parseInt(offset)/pageSize)+1;
+			PageInfo<PersonVo> userList= personService.findUserPage(personVo,pageNum,pageSize);
+			
+			
+			JSONObject jsonObj = new JSONObject();
+			jsonObj.put("total",userList.getTotal() );
+			jsonObj.put("rows", userList.getList());
+			WriterUtil.write(response,jsonObj.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("用户展示错误",e);
+			throw e;
+		}
+	}
+	
+	
+	// 新增或修改
+	@RequestMapping("reservePerson")
+	public void reserveUser(HttpServletRequest request,Person person,HttpServletResponse response){
+		Integer userId = person.getId();
+		JSONObject result=new JSONObject();
+		try {
+			if (userId != null) {   // userId不为空 说明是修改
+				
+					person.setId(userId);
+					personService.updateUser(person);
+					result.put("success", true);
+				
+				
+			}else {   // 添加
+				
+				personService.addUser(person);
+					result.put("success", true);
+				
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("保存用户信息错误",e);
+			result.put("success", true);
+			result.put("errorMsg", "对不起，操作失败");
+		}
+		WriterUtil.write(response, result.toString());
+	}
+	
+	
+	@RequestMapping("deleteUser")
+	public void delUser(HttpServletRequest request,HttpServletResponse response){
+		JSONObject result=new JSONObject();
+		try {
+			String[] ids=request.getParameter("ids").split(",");
+			for (String id : ids) {
+				personService.deleteUser(Integer.parseInt(id));
+			}
+			result.put("success", true);
+			result.put("delNums", ids.length);
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("删除用户信息错误",e);
+			result.put("errorMsg", "对不起，删除失败");
+		}
+		WriterUtil.write(response, result.toString());
+	}
+	//报表
+		@RequestMapping("getEcharts")
+		public void getEcharts(HttpServletRequest request,HttpServletResponse response){
+			JSONObject result=new JSONObject();
+			try {
+				//查询报表
+				List<PersonVo> plist = personService.getEcharts();
+				result.put("plist", plist);
+				result.put("success", true);
+			} catch (Exception e) {
+				e.printStackTrace();
+				logger.error("删除用户信息错误",e);
+				result.put("errorMsg", "对不起，删除失败");
+			}
+			WriterUtil.write(response, result.toString());
+		}
+		
+
+
+}
